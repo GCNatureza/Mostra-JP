97bb0e3481cd4dff771b93f40c6cbdd593dd8787
GCNatureza <prof.alex.vieira.fisica@gmail.com>
2026-09-21 18:19:47 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* Endereço do app da web do Apps Script (termina em /exec). */
 window.CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbztL8EbltzKXUFtJPziyJr3x7_XjrSlNdOMeGD5EvrlprQ2UmvMSE7Jp8ZR8eGBGmk/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxkiRBfScUYCjydZcFVBq9obHXqRSX93feNBJflpCL1vPdLBgktn69gf9kkpGmzq4Jj/exec",
 };
