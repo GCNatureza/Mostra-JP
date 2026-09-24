17c5eef5875808c1498811533987eb32685e5221
GCNatureza <prof.alex.vieira.fisica@gmail.com>
2026-09-24 05:25:56 -0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* Endereço do app da web do Apps Script (termina em /exec). */
 window.CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzk3Cfoga2_7cl9HMxrLMxvR9M2z8GOONC2wkSYvE3kv0WlqjHT8hR0aGKShiMfzYEi/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbx3tGJhRkYFZ0fVXPIjCXATgllSr2Z7_9CRrEeFrH3iCxiK55k3Y5WG7WRpKrlBtq0t/exec",
 };
